0b9ad762e07d2d847e7af59aee477a43f5d483bf
Soumyadip Bhattacharjya <101652501+SoumyaCO@users.noreply.github.com>
2024-09-11 10:26:08 +0530

    init

---
+++ wezterm/.config/wezterm/helpers.lua
@@ -0,0 +1,42 @@
+local wezterm = require("wezterm")
+
+-- module table for exporting
+local module = {}
+
+-- private function (not visible from outside)
+local function private_helper()
+	wezterm.log_error("hello!")
+end
+
+-- define a function in the module table.
+-- Only function defined in `module` will be exported to code
+-- that imports this module
+
+function module.apply_to_config(config)
+	private_helper()
+
+	config.font = wezterm.font_with_fallback({
+		"Dank Mono",
+		"Agave Nerd Font",
+	})
+	config.font_size = 18
+
+	config.enable_tab_bar = false
+	config.window_decorations = "RESIZE"
+
+	config.window_background_opacity = 1.0
+	config.text_background_opacity = 1.0
+
+	config.window_background_image_hsb = {
+		-- Darken the background image
+		brightness = 1.0,
+		-- hue
+		hue = 1.0,
+		--saturation
+		saturation = 1.0,
+	}
+	config.color_scheme = "Catppuccin Mocha"
+end
+
+-- return module table
+return module
