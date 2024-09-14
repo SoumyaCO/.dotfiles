2302c58580a16fe14840546273fbf1d06b387a9e
Soumyadip Bhattacharjya <101652501+SoumyaCO@users.noreply.github.com>
2024-09-15 00:56:11 +0530

    new(sketchybar): borders and sketchybar with aerospace

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1076,6 +1076,31 @@ require('lazy').setup({
       require('dbee').setup(--[[optional config]])
     end,
   },
+  {
+    'prichrd/netrw.nvim',
+    opts = {},
+    config = function()
+      require('netrw').setup {
+        -- File icons to use when `use_devicons` is false or if
+        -- no icon is found for the given file type.
+        icons = {
+          symlink = '',
+          directory = '',
+          file = '',
+        },
+        -- Uses mini.icon or nvim-web-devicons if true, otherwise use the file icon specified above
+        use_devicons = true,
+        mappings = {
+          -- Function mappings receive an object describing the node under the cursor
+          ['p'] = function(payload)
+            print(vim.inspect(payload))
+          end,
+          -- String mappings are executed as vim commands
+          ['<Leader>p'] = ":echo 'hello world'<CR>",
+        },
+      }
+    end,
+  },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
